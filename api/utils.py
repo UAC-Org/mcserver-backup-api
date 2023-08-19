9eb1488e0376022c8415bfe814b37f903626abfa
Ricky8955555 <rkmiao@duck.com>
2023-08-19 13:59:33 +0800

    Release temp file

--- api/utils.py
+++ api/utils.py
@@ -19,7 +19,7 @@ def generate_tar_archive(archive_path: str, *sources: str):
         with tarfile.open(archive_path, "w") as archive:
             for source in sources:
                 archive.add(source, arcname=os.path.split(source)[1])
-    except:
+    except Exception:
         _try_remove(archive_path)
     return archive_path
 
@@ -32,17 +32,15 @@ def generate_zstd_compressed_tar_archive(archive_path: str, *sources: str):
     import zstandard
 
     compressor = zstandard.ZstdCompressor()
-    _, tar_archive = tempfile.mkstemp()
+    fd, tar_archive = tempfile.mkstemp()
     try:
         with tarfile.open(tar_archive, "w") as archive:
             for source in sources:
                 archive.add(source, arcname=os.path.split(source)[1])
         with open(tar_archive, "rb") as tar, open(archive_path, "wb") as zstd:
             compressor.copy_stream(tar, zstd)
-    except Exception:
-        _try_remove(archive_path)
-        raise
     finally:
+        os.close(fd)
         _try_remove(tar_archive)
     return archive_path
 
